ed9e3b1101cc7410b918cb2cf957c5f71bbd1727
Shadab-2604 <138616478+Shadab-2604@users.noreply.github.com>
2025-02-22 01:44:50 +0530

    vercel.json

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
